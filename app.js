08b3afaf07fb9c0b2a1a21ce4a2e68cc5185505f
Nehal Ali <nehalalikamran103@gmail.com>
2025-11-26 02:49:46 -0800

    Assignment 12 complete 13 work

---
+++ app.js
@@ -0,0 +1,43 @@
+// // Chapter 12
+// // Q no 1
+// var topCity = prompt("Pakistan Ka Top City Number 1")
+// if (topCity.toLowerCase() === "karachi"){
+//     console.log("Correct");
+// };
+// if ( topCity != "karachi" ){
+//     console.log("Wrong answer")
+// }
+
+
+// // Q no 2
+// var topCity = prompt("Pakistan Ka Top City Number 1")
+// if (topCity.toLowerCase() === "karachi"){
+//     console.log("Correct");
+// }
+
+// else {
+//     console.log("Wrong answer")
+// }
+
+
+// // Qno 3
+// var Safety = prompt("Driving Karte hue Safety Bike and Car \nCar Safety , Seatbelt , Speed limits \nBike Safety Helmet");
+
+// if ( Safety.toLowerCase() === "seatbelt" ) {
+//     console.log("Always wear your sealtbelt before driving")    
+// }
+
+// else if (Safety.toLowerCase() === "speed limit") {
+//     console.log("Obey all posted speed limit")
+// }
+
+// else if ( Safety.toLowerCase() === "helmet" ) {
+//     console.log("The rider and passenger must always weara properly fitted helmet")
+// }
+
+// else{
+//     console.log("Error: Invalid Rule. Please enter Bike and Car Safety")
+// }
+
+// Chapter 13
+// Q no 1
