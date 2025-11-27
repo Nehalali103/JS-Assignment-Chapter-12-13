4e814c2efe2cee37e6380fb3dd4f7e82bf5d889d
Nehal Ali <nehalalikamran103@gmail.com>
2025-11-27 02:17:59 -0800

    13 Assignmnet complete

--- app.js
+++ app.js
@@ -41,3 +41,41 @@
 
 // Chapter 13
 // Q no 1
+// var value_1 = prompt("Enter your Frist number");
+// var value_2 = prompt("Enter your second number");
+
+// if ( value_1 > value_2) {
+//   console.log("The large first number " + value_1);
+// }
+
+// else if ( value_1 < value_2) {
+//   console.log("The large secnod number " + value_2);
+// }
+
+
+// Q no 2
+// var institute1 = "smit";
+// var institute2 = 'saylani mass it training';
+// var askinstitute1 = prompt("Enter your institute Smit ya Saylani Mass IT Training💻✅:");
+
+
+// if (askinstitute1.toLowerCase() == institute1 || askinstitute1.toLowerCase() == institute2 ) {
+//     alert(askinstitute1 + " Mass IT Training ✅");
+// }
+
+// else {
+//     alert("any another institute . . . . . . ❌")
+// };
+
+
+// Q no 3
+// var placeName = prompt("Enter your place name");
+// var age = +prompt("Enter are age");
+
+// if ( placeName != "" && age != "" ) {
+//     console.log("you are eiligble")
+// }
+
+// else {
+//     console.log("not are eiligble")
+// }
